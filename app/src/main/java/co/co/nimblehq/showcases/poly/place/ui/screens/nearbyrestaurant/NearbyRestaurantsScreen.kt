b2e3fc7cb5a324ca353711944d271ade7c1ca81f
Bruce - Thien Nguyen <thien@nimblehq.co>
2023-03-03 16:28:50 +0700

    #5 Create UI model and clean up code

--- app/src/main/java/co/co/nimblehq/showcases/poly/place/ui/screens/nearbyrestaurant/NearbyRestaurantsScreen.kt
+++ app/src/main/java/co/co/nimblehq/showcases/poly/place/ui/screens/nearbyrestaurant/NearbyRestaurantsScreen.kt
@@ -1,31 +1,28 @@
 package co.co.nimblehq.showcases.poly.place.ui.screens.nearbyrestaurant
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
 import androidx.hilt.navigation.compose.hiltViewModel
 import co.co.nimblehq.showcases.poly.place.R
 import co.co.nimblehq.showcases.poly.place.model.UiModel
 import co.co.nimblehq.showcases.poly.place.ui.AppDestination
+import co.co.nimblehq.showcases.poly.place.ui.screens.nearbyrestaurant.uimodel.Restaurant
 import co.co.nimblehq.showcases.poly.place.ui.theme.*
 import co.co.nimblehq.showcases.poly.place.ui.theme.AppTheme.dimensions
-import com.google.android.libraries.places.api.model.PlaceLikelihood
 import timber.log.Timber
 
 @Composable
@@ -41,39 +38,118 @@ fun NearbyRestaurantsScreen(
 }
 
 @Composable
-fun NearbyRestaurantsToolbar() {
+private fun NearbyRestaurantsContent(
+    uiModels: List<UiModel>
+) {
+    // Mock data for UI task
+    // TODO: Remove this when implementing integrate task: https://github.com/nimblehq/poly-place/issues/32
+    val mockRestaurants = arrayListOf(
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        ),
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        ),
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        ),
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        ),
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        ),
+        Restaurant(
+            thumbnailImage = ContextCompat.getDrawable(
+                LocalContext.current,
+                R.drawable.im_restaurant_mock
+            )!!.toBitmap(),
+            name = "Dragon X",
+            address = "111, Dr. Strange street",
+            distance = "50 m"
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.bg_nearby_restaurants),
+                contentScale = ContentScale.FillBounds
+            )
+    ) {
+        NearbyRestaurantsToolbar()
+        NearbyRestaurantList(mockRestaurants)
+    }
+    Timber.d("Result : $uiModels")
+}
+
+@Composable
+private fun NearbyRestaurantsToolbar() {
     ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
         val (title, iconEnd, divider) = createRefs()
 
-        val spacingSmall = dimensions.spacingSmall
         val spacingNormal = dimensions.spacingNormal
         val spacingLarge = dimensions.spacingLarge
         val spacingHuge = dimensions.spacingHuge
 
         Text(
-            text = stringResource(R.string.nearby_restaurants_title),
-            color = Color.White,
-            fontSize = dimensions.textSizeToolbar,
             modifier = Modifier
                 .constrainAs(title) {
                     top.linkTo(parent.top, spacingHuge)
                     start.linkTo(parent.start, spacingNormal)
-                }
+                },
+            text = stringResource(R.string.nearby_restaurants_title),
+            color = Color.White,
+            fontSize = dimensions.textSizeToolbar
         )
         Icon(
-            painter = painterResource(id = R.drawable.ic_target),
-            contentDescription = null,
-            tint = BlueDenim,
             modifier = Modifier
-                .size(25.dp)
+                .size(dimensions.iconSizeToolbar)
                 .constrainAs(iconEnd) {
                     end.linkTo(parent.end, spacingLarge)
                     centerVerticallyTo(title)
-                }
+                },
+            painter = painterResource(id = R.drawable.ic_target),
+            contentDescription = null,
+            tint = BlueDenim
         )
         Divider(
             modifier = Modifier
-                .padding(horizontal = spacingNormal, vertical = spacingSmall)
+                .padding(
+                    horizontal = dimensions.spacingNormal,
+                    vertical = dimensions.spacingSmall
+                )
                 .constrainAs(divider) {
                     top.linkTo(title.bottom)
                     centerHorizontallyTo(parent)
@@ -84,32 +160,10 @@ fun NearbyRestaurantsToolbar() {
 }
 
 @Composable
-private fun NearbyRestaurantsContent(
-    uiModels: List<UiModel>
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .paint(
-                painter = painterResource(id = R.drawable.bg_nearby_restaurants),
-                contentScale = ContentScale.FillBounds
-            )
-    ) {
-        NearbyRestaurantsToolbar()
-        NearbyRestaurantList(emptyList())
-
-    }
-    Timber.d("Result : $uiModels")
-}
-
-@Composable
 private fun NearbyRestaurantList(
-    restaurants: List<PlaceLikelihood>
+    restaurants: List<Restaurant>
 ) {
     LazyVerticalGrid(
-        verticalArrangement = Arrangement.spacedBy(dimensions.spacingNormal),
-        horizontalArrangement = Arrangement.spacedBy(dimensions.spacingNormal),
-        columns = GridCells.Fixed(2),
         modifier = Modifier
             .fillMaxWidth()
             .padding(
@@ -117,10 +171,13 @@ private fun NearbyRestaurantList(
                 end = dimensions.spacingNormal,
                 top = dimensions.spacingNormal,
                 bottom = dimensions.spacingHuge
-            )
+            ),
+        verticalArrangement = Arrangement.spacedBy(dimensions.spacingNormal),
+        horizontalArrangement = Arrangement.spacedBy(dimensions.spacingNormal),
+        columns = GridCells.Fixed(2)
     ) {
-        items(5) {
-            NearbyRestaurantItem()
+        items(restaurants.size) { index ->
+            NearbyRestaurantItem(restaurants[index])
         }
     }
 }
