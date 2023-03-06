86585ebbfe5a5b8a8faeb03ef8a51b5c716ac16d
Bruce - Thien Nguyen <thien@nimblehq.co>
2023-03-06 15:53:21 +0700

    #5 Add content scale to thumbnail image and key item for lazygrid

--- app/src/main/java/co/co/nimblehq/showcases/poly/place/ui/screens/restaurant/uimodel/Restaurant.kt
+++ app/src/main/java/co/co/nimblehq/showcases/poly/place/ui/screens/restaurant/uimodel/Restaurant.kt
@@ -3,6 +3,7 @@ package co.co.nimblehq.showcases.poly.place.ui.screens.restaurant.uimodel
 import android.graphics.Bitmap
 
 data class Restaurant(
+    val id: String,
     val thumbnailImage: Bitmap,
     val name: String,
     val address: String,
